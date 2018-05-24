6f765de3e11fab746f2c417681496bf82c17ef6f
Tushar Kant Kar <tushar.et1@gmail.com>
2018-05-24 18:44:37 +0530

    Commiting the new FrontEnd Code
    Commiting the new frontend code

---
+++ frontend/src/Login.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+var apiBaseUrl = "http://localhost:4000/api/";
+import axios from 'axios';
+import UploadPage from './UploadPage';
+class Login extends Component {
+  constructor(props){
+    super(props);
+    var localloginComponent=[];
+    localloginComponent.push(
+      <MuiThemeProvider>
+        <div>
+         <TextField
+           hintText="Enter your Email Id"
+           floatingLabelText="Email Id"
+           onChange = {(event,newValue)=>this.setState({username:newValue})}
+           />
+         <br/>
+           <TextField
+             type="password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+       </div>
+       </MuiThemeProvider>
+    )
+    this.state={
+      username:'',
+      password:'',
+      loginComponent:localloginComponent,
+    }
+  }
+  componentWillMount(){
+      var localloginComponent=[];
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your Email Id"
+             floatingLabelText="Email Id"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+      this.setState({menuValue:1,loginComponent:localloginComponent})
+  }
+  handleClick(event){
+    var self = this;
+    var payload={
+      "userid":this.state.username,
+	    "password":this.state.password
+    }
+    axios.post(apiBaseUrl+'login', payload)
+   .then(function (response) {
+     console.log(response);
+     if(response.data.code == 200){
+       console.log("Login successfull");
+       var uploadScreen=[];
+       uploadScreen.push(<UploadPage appContext={self.props.appContext}/>)
+       self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+     }
+     else if(response.data.code == 204){
+       console.log("Username password do not match");
+       alert(response.data.success)
+     }
+     else{
+       console.log("Username does not exists");
+       alert("Username does not exist");
+     }
+   })
+   .catch(function (error) {
+     console.log(error);
+   });
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+        <AppBar
+             title="Login"
+           />
+        </MuiThemeProvider>
+        {this.state.loginComponent}
+      </div>
+    );
+  }
+}
+
+const style = {
+  margin: 15,
+};
+
+export default Login;
